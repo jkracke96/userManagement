534c3243057aea2e7061bbec7687861357db0927
jkracke96 <80530341+jkracke96@users.noreply.github.com>
2023-12-26 11:30:45 +0100

    update user credentials to env

--- website/website/settings.py
+++ website/website/settings.py
@@ -9,8 +9,10 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-wl+om&xwbitu!#t1k@!@w8y-=s#14+i^*6bprd507r$w%w$ke8'
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -77,17 +79,13 @@ WSGI_APPLICATION = 'website.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-from . import credentials_local
-
-db_credentials = credentials_local.credentials["postgres_db"]
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'd80s5u6r2mftue',
-        'USER': db_credentials['USER'],
-        'PASSWORD': db_credentials['PW'],
-        'HOST': 'ec2-34-242-154-118.eu-west-1.compute.amazonaws.com',
+        'NAME': str(os.getenv('DB_NAME')),
+        'USER': str(os.getenv('DB_USER')),
+        'PASSWORD': str(os.getenv('DB_PW')),
+        'HOST': str(os.getenv('DB_HOST')),
         'PORT': '5432'
     }
 }
